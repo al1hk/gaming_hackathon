36c05c353e1d248ac4009bb0934f32a9e2b015b7
al1hk <160332226+al1hk@users.noreply.github.com>
2025-01-28 01:22:30 +0500

    Update structure.ts

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,11 +1,7 @@
-import {StructureBuilder} from 'sanity/desk'
+import type {StructureResolver} from 'sanity/structure'
 
-export const defaultStructure = (S: StructureBuilder) =>
+// https://www.sanity.io/docs/structure-builder-cheat-sheet
+export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
-    .items([
-      S.documentTypeListItem('post').title('Posts'), // Customize with your schema types
-      S.documentTypeListItem('author').title('Authors'),
-      S.divider(),
-      S.documentTypeListItem('category').title('Categories'),
-    ])
+    .items(S.documentTypeListItems())
