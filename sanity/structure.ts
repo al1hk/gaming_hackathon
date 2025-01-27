05605cf6f62938402b546b259f6ae32515d5e554
al1hk <160332226+al1hk@users.noreply.github.com>
2025-01-27 21:01:52 +0500

    Update structure.ts

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,8 +1,7 @@
 import type {StructureResolver} from 'sanity/structure'
+
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-const structure: StructureResolver = (S) =>
+export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
     .items(S.documentTypeListItems())
-
-export default structure
